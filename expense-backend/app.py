0486c47869711a289a3278a576883a55f79ce41d
Shabbir Khan <shabbirkhan@Mac.lan>
2025-07-18 22:47:00 -0500

    Fix for Render backend

--- expense-backend/app.py
+++ expense-backend/app.py
@@ -38,4 +38,5 @@ def log_expense():
     return jsonify({'message': 'Expense saved successfully'}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
